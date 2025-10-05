50fc8f88cd6f8cd722ea6aaaa314cb29564dc8db
Aditya <adityakul3333@gmail.com>
2025-10-05 16:27:30 +0530

    adding new service connection and adding all files last try for today

--- main.tf
+++ main.tf
@@ -1,8 +1,8 @@
 module "keyvault" {
   source              = "./module/keyvault" # Update this to the correct relative path of your module
-  resource_group_name = "rg-iac-demo"
-  location            = "East US"
-  key_vault_name      = "kv-iac-demo"
+  resource_group_name = var.resource_group_name
+  location            = var.location
+  key_vault_name      = var.key_vault_name
   tenant_id           = var.tenant_id
   subscription_id     = var.subscription_id
   client_id           = var.client_id
