baf697589a014bea88ab9562f16b729d589e221c
Aditya <adityakul3333@gmail.com>
2025-10-05 06:34:09 +0530

    adding new service connection and adding all files last try for today

--- provider.tf
+++ provider.tf
@@ -8,9 +8,9 @@ terraform {
 }
 
 provider "azurerm" {
-  client_id       = var.client_id
-  client_secret   = var.client_secret
-  tenant_id       = var.tenant_id
-  subscription_id = var.subscription_id
+client_id        = "e83c5716-aaf5-4fa8-8c9b-0e0702d3f769"
+client_secret    = "ea55f29e-f71f-41e0-befb-ad2583062052"
+tenant_id        = "c6d86902-c35f-4598-8508-e9eadbf89be8"
+subscription_id  = "2583c16b-f24c-49eb-aecc-3d7430de7f47"
 
 }
