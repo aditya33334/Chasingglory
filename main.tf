687f077abc5fbf9101cad945751c1070518d7abf
Aditya <adityakul3333@gmail.com>
2025-10-05 22:00:47 +0530

    adding new service connection and adding all files last try for today

--- main.tf
+++ main.tf
@@ -1,5 +1,5 @@
 module "resourcegroup" {
-  source = ".\resourcegroups"
+  source = "./modules/resourcegroups"
     azurerm_resource_group_name     = var.azurerm_resource_group_name
     azurerm_resource_group_location = var.azurerm_resource_group_location
 }
